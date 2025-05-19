489a0c36ad6dd0e88b6b0b11a7413f282a1a3cbd
thug-han <hjl999@126.com>
2025-05-19 17:21:05 +0800

    添加Compose界面 增加各module变量逻辑在lib和application变换

---
+++ src/main/java/com/thughan/kotlin/KotlinConstants.java
@@ -0,0 +1,9 @@
+package com.thughan.kotlin;
+
+public class KotlinConstants {
+    public static final String ACTIVITY_COMPOSE_PATH = "/kotlin/compose";
+    public static final String ACTIVITY_COMPOSE_NAME = "compose模块";
+
+    public static final String ACTIVITY_FIRST_LINE_PATH = "/kotlin/firstline";
+    public static final String ACTIVITY_FIRST_LINE_NAME = "FIRST_LINE模块";
+}
--- src/main/java/com/thughan/kotlin/firstline/MainActivity.kt
+++ src/main/java/com/thughan/kotlin/firstline/MainActivity.kt
@@ -2,8 +2,11 @@ package com.thughan.kotlin.firstline
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.alibaba.android.arouter.facade.annotation.Route
+import com.thughan.kotlin.KotlinConstants
 import com.thughan.kotlin.R
 
+@Route(path = KotlinConstants.ACTIVITY_FIRST_LINE_PATH)
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
